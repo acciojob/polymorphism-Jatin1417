96b2facbba402b10cae58465c4280fbf689d8c6e
Jatin1417 <chaudhari.jatin.18ee1164@gmail.com>
2022-12-10 23:49:19 +0530

    second commit

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,6 +3,15 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
+import com.driver.Main;
+import com.driver.Main.Product;
+
 public class TestCases {
-     
+   public static void main(String args[]){
+    Product p = new Product();
+    System.out.println(p.product(2,4));
+    System.out.println(p.product(2,4,6));
+    System.out.println(p.product(2.0,4.0));
+   }
+
 }
